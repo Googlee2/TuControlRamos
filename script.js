ae06ea9891a9452a7fb4a3ac3f072b5d69c39f69
Googlee2 <soyjuanwebb@yahoo.com>
2026-01-30 13:49:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -111,3 +111,30 @@ async function cargarGaleria() {
  * CARGA INICIAL
  ********************************/
 cargarGaleria();
+const BACKEND_URL = 'https://tucontrolramos-1.onrender.com';
+
+function pagar() {
+  fetch(`${BACKEND_URL}/create-preference`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      items: [{
+        title: 'Control remoto',
+        quantity: 1,
+        unit_price: 8500
+      }]
+    })
+  })
+  .then(res => res.json())
+  .then(data => {
+    if (data.init_point) {
+      window.location.href = data.init_point;
+    } else {
+      alert('No se pudo iniciar el pago');
+    }
+  })
+  .catch(err => {
+    console.error(err);
+    alert('Error de conexión');
+  });
+}
